01ad2e74830c8e6b193266a09c7a63d80afc9bc2
naoshi2 <naoc007t@yahoo.co.jp>
2017-09-02 00:58:14 +0900

    Add horizontal pane resize

--- resize.js
+++ resize.js
@@ -2,3 +2,8 @@ $(".panel-left").resizable({
     handleSelector: ".splitter",
     resizeHeight: false
 });
+
+$(".panel-top").resizable({
+    handleSelector: ".splitter-horizontal",
+    resizeWidth: false
+});
