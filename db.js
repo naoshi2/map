5d215299ba8c87e1f0eab810a1bcd5090e9e5f15
naoshi2 <naoc007t@yahoo.co.jp>
2017-09-11 00:15:44 +0900

    Update sql on db.js

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db/db.sqlite3');
 
-db.run("INSERT INTO messages (content) VALUES (?)", "aaa");
+db.all("SELECt * FROM tweet ORDER BY timestamp desc LIMIT 3", function (err, rows) {
+    rows.forEach(function (row) {
+        console.log(row.timestamp);
+        console.log(row.name);
+        console.log(row.content);
+        console.log("-------");
+    }, this);
+})
+
 db.close();
